29beb0d91820abf61e9fcdeb8f687f81e52abee0
nurilloh2004 <nurillohpython@gmail.com>
2023-01-11 10:08:30 +0500

    Initial implementation of Connect Four game.

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 import { createBoard, playMove } from "./connect4.js";
 
 
+function initGame(websocket) {
+
+  websocket.addEventListener("open", () => {
+
+    // Send an "init" event according to who is connecting.
+
+    const params = new URLSearchParams(window.location.search);
+
+    let event = { type: "init" };
+
+    if (params.has("join")) {
+
+      // Second player joins an existing game.
+
+      event.join = params.get("join");
+
+    } else if (params.has("watch")) {
+
+      // Spectator watches an existing game.
+
+      event.watch = params.get("watch");
+
+    } else {
+
+      // First player starts a new game.
+
+    }
+
+    websocket.send(JSON.stringify(event));
+
+  });
+
+}
+
+
 function showMessage(message) {
 
   window.setTimeout(() => window.alert(message), 50);
@@ -15,6 +50,16 @@ function receiveMoves(board, websocket) {
     const event = JSON.parse(data);
 
     switch (event.type) {
+
+      case "init":
+
+        // Create links for inviting the second player and spectators.
+
+        document.querySelector(".join").href = "?join=" + event.join;
+
+        document.querySelector(".watch").href = "?watch=" + event.watch;
+
+        break;
 
       case "play":
 
@@ -52,6 +97,17 @@ function receiveMoves(board, websocket) {
 
 
 function sendMoves(board, websocket) {
+
+  // Don't send moves for a spectator watching a game.
+
+  const params = new URLSearchParams(window.location.search);
+
+  if (params.has("watch")) {
+
+    return;
+
+  }
+
 
   // When clicking a column, send a "play" event for a move in that column.
 
@@ -93,6 +149,8 @@ window.addEventListener("DOMContentLoaded", () => {
   // Open the WebSocket connection and register event handlers.
 
   const websocket = new WebSocket("ws://localhost:8001/");
+
+  initGame(websocket);
 
   receiveMoves(board, websocket);
 
